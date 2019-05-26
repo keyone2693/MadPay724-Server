5017798dacd0d576f240db1d65b6200a144e37e9
keyone2693 <key.one72@gmail.com>
2019-05-27 00:26:53 +0430

    S04-E03

---
+++ MadPay724.Data/Infrastructure/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadPay724.Data.Infrastructure
+{
+    public interface IUnitOfWork<TContext> : IDisposable where TContext:DbContext
+    {
+        void Save();
+        Task<int> SaveAsync();
+
+    }
+}
---
+++ MadPay724.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadPay724.Data.Infrastructure
+{
+    public class UnitOfWork<TContext> : IUnitOfWork<TContext> where TContext : DbContext, new()
+    {
+        #region ctor
+        protected readonly DbContext _db;
+        public UnitOfWork()
+        {
+            _db = new TContext();
+        }
+        #endregion
+
+        #region save
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public Task<int> SaveAsync()
+        {
+            return _db.SaveChangesAsync();
+        }
+
+        #endregion
+
+        #region dispose
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _db.Dispose();
+                }
+            }
+            disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~UnitOfWork()
+        {
+            Dispose(false);
+        }
+        #endregion
+    }
+}
