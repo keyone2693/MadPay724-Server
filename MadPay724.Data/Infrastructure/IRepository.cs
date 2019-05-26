c676c795ffdcec7390b5e00628519ca41c13e65e
keyone2693 <key.one72@gmail.com>
2019-05-27 00:27:38 +0430

    S04-E04

---
+++ MadPay724.Data/Infrastructure/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadPay724.Data.Infrastructure
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(object id);
+        void Delete(TEntity entity);
+        void Delete(Expression<Func<TEntity,bool>> where);
+
+
+        TEntity GetById(object id);
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(Expression<Func<TEntity, bool>> where);
+        IEnumerable<TEntity> GetMany(Expression<Func<TEntity, bool>> where);
+
+        //--------------------------------------------
+
+        Task InsertAsync(TEntity entity);
+
+        Task<TEntity> GetByIdAsync(object id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> where);
+        Task<IEnumerable<TEntity>> GetManyAsync(Expression<Func<TEntity, bool>> where);
+    }
+}
---
+++ MadPay724.Data/Infrastructure/Repository.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadPay724.Data.Infrastructure
+{
+    public class Repository<TEntity> : IRepository<TEntity>, IDisposable where TEntity : class
+    {
+        #region ctor
+        private readonly DbContext _db;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public Repository(DbContext db)
+        {
+            _db = db;
+            _dbSet = _db.Set<TEntity>();
+        }
+        #endregion
+
+        #region normal 
+        public void Insert(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+        public void Update(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentException("there is no entity");
+            _dbSet.Update(entity);
+        }
+        public void Delete(object id)
+        {
+            var entity = GetById(id);
+            if (entity == null)
+                throw new ArgumentException("there is no entity");
+            _dbSet.Remove(entity);
+        }
+        public void Delete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+        public void Delete(Expression<Func<TEntity, bool>> where)
+        {
+            IEnumerable<TEntity> objs = _dbSet.Where(where).AsEnumerable();
+            foreach (TEntity item in objs)
+            {
+                _dbSet.Remove(item);
+            }
+        }
+        public TEntity GetById(object id)
+        {
+            return _dbSet.Find(id);
+        }
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _dbSet.AsEnumerable();
+        }
+        public TEntity Get(Expression<Func<TEntity, bool>> where)
+        {
+            return _dbSet.Where(where).FirstOrDefault();
+        }
+        public IEnumerable<TEntity> GetMany(Expression<Func<TEntity, bool>> where)
+        {
+            return _dbSet.Where(where).AsEnumerable();
+        }
+        #endregion
+
+        #region async
+        public async Task InsertAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task<TEntity> GetByIdAsync(object id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> where)
+        {
+            return await _dbSet.Where(where).FirstOrDefaultAsync();
+        }
+        public async Task<IEnumerable<TEntity>> GetManyAsync(Expression<Func<TEntity, bool>> where)
+        {
+            return await _dbSet.Where(where).ToListAsync();
+        }
+
+
+
+        #endregion
+
+        #region dispose
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _db.Dispose();
+                }
+            }
+            disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~Repository()
+        {
+            Dispose(false);
+        }
+
+        #endregion
+    }
+}
