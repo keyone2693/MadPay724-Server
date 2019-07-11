5f4d56dc1c939f7ac538698aaa819ea8524e5abd
Key1-lab <key.one72@gmail.com>
2019-07-11 18:23:20 +0430

    S15-E04

---
+++ MadPay724.Test/TestClientProvider.cs
@@ -0,0 +1,22 @@
+﻿using MadPay724.Presentation;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace MadPay724.Test
+{
+    public class TestClientProvider
+    {
+        public HttpClient _client;
+        public TestClientProvider()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+
+            _client = server.CreateClient();
+
+        }
+    }
+}
