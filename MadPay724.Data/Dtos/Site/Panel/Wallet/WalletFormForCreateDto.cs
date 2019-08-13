2f195614c796fe4da10a55ffd2e563d524249b04
Key1-lab <key.one72@gmail.com>
2019-08-13 12:02:04 +0430

    S23-E11

---
+++ MadPay724.Data/Dtos/Site/Panel/Wallet/WalletFormForCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MadPay724.Data.Dtos.Site.Panel.Wallet
+{
+   public class WalletFormForCreateDto
+    {
+        public string walletId { get; set; }
+        public string walletName { get; set; }
+    }
+}
