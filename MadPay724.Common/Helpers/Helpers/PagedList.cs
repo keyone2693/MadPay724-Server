7feadf594bed655aafea16e48d5d73d209fe096c
key.one72@gmail.com <key.one72@gmail.com>
2019-11-25 11:42:27 +0330

    S29-E02

---
+++ MadPay724.Common/Helpers/Helpers/PagedList.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadPay724.Common.Helpers.Helpers
+{
+    public class PagedList<T> : List<T>
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public PagedList(List<T> items,int count,int pageNumber,int pageSize)
+        {
+            TotalCount = count;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+            TotalPage = (int)Math.Ceiling(count / (double)pageSize);
+            this.AddRange(items);
+        }
+
+
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source,
+            int pageNumber, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var item = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PagedList<T>(item, count, pageNumber, pageSize);
+        }
+
+    }
+}
