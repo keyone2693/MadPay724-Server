db88512250d530513f8ccc52c52b8df2e8ca0cce
key.one72@gmail.com <key.one72@gmail.com>
2020-01-28 14:11:05 +0330

    S44-E07

--- MadPay724.Services/Site/Panel/Common/Service/ChatHubService.cs
+++ MadPay724.Services/Site/Panel/Common/Service/ChatHubService.cs
@@ -1,19 +1,59 @@
 ﻿
+using MadPay724.Common.OnlineChat.Storage;
 using MadPay724.Services.Site.Panel.Common.Interface;
 using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MadPay724.Services.Site.Panel.Common.Service
 {
-    public class ChatHubService : Hub<IChatClient>
+    public class ChatHubService : Hub
     {
-        public Task SendMessage(string user, string message)
+        private readonly UserInfoInMemory _userInfoInMemory;
+        private readonly string _name;
+        private readonly string _connectionId;
+        public ChatHubService(UserInfoInMemory userInfoInMemory)
         {
-            return Clients.All.ReceiveMessage(user, message);
+            _userInfoInMemory = userInfoInMemory;
+            _name = Context.User.Identity.Name;
+            _connectionId = Context.ConnectionId;
         }
-        public Task SendMessageToCaller(string message)
+
+        public async Task LeaveAsync()
         {
-            return Clients.Caller.ReceiveMessage(message);
+            _userInfoInMemory.Remove(_name);
+            await Clients.AllExcept(new List<string> { _connectionId })
+                .SendAsync("UserLeft", _name);
+        }
+        public async Task JoinAsync()
+        {
+            if (!_userInfoInMemory.AddUpdate(_name, _connectionId))
+            {
+                // new user
+
+                var list = _userInfoInMemory.GetAllUsersExceptThis(_name).ToList();
+                await Clients.AllExcept(new List<string> { _connectionId })
+                    .SendAsync("NeOnlineUser", _userInfoInMemory.GetUserInfo(_name));
+            }
+            else
+            {
+                //existing user joined again
+            }
+
+            await Clients.Client(_connectionId)
+                .SendAsync("Joined", _userInfoInMemory.GetUserInfo(_name));
+
+            await Clients.Client(_connectionId)
+                .SendAsync("OnlineUsers", _userInfoInMemory.GetAllUsersExceptThis(_name));
+        }
+
+        public Task SendDirectMessage(string message, string targetuserName)
+        {
+            var userInfoSender = _userInfoInMemory.GetUserInfo(_name);
+            var userInfoReciever = _userInfoInMemory.GetUserInfo(targetuserName);
+
+            return Clients.Client(userInfoReciever.ConnectionId).SendAsync("SendDirectMessage",message, userInfoSender);
         }
     }
 }
