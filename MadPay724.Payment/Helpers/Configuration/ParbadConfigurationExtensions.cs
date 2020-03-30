542b2b11bad13f7a42a4b3850fd33c8f076ccaee
key.one72@gmail.com <key.one72@gmail.com>
2020-03-30 13:39:13 +0430

    S50-E09

---
+++ MadPay724.Payment/Helpers/Configuration/ParbadConfigurationExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Parbad.Builder;
+
+namespace MadPay724.Payment.Helpers.Configuration
+{
+    public static class ParbadConfigurationExtensions
+    {
+        public static void AddMadParbad(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddParbad()
+                .ConfigureGateways(gateWayes =>
+                {
+                    gateWayes
+                    .AddMellat()
+                    .WithAccounts(accs =>
+                    {
+                        accs.AddFromConfiguration(configuration.GetSection("MellatBank"));
+                    });
+                    gateWayes
+                    .AddZarinPal()
+                    .WithAccounts(accs =>
+                    {
+                        accs.AddFromConfiguration(configuration.GetSection("ZarinPalBank"));
+                    });
+                    gateWayes
+                    .AddParbadVirtual()
+                    .WithOptions(bld => bld.GatewayPath = "/MadpayGateWay");
+                })
+                .ConfigureHttpContext(bld => bld.UseDefaultAspNetCore())
+                .ConfigureStorage(bld =>
+                {
+                    bld.UseEntityFrameworkCore(ef =>
+                        ef.UseSqlServer(@"Data Source=KEY1-LAB\MSSQLSERVER2016;Initial Catalog=Financial_MadPay724db;Integrated Security=True;MultipleActiveResultSets=True;",
+                        opt => opt.UseParbadMigrations()))
+                    .ConfigureDatabaseInitializer(bld =>
+                    {
+                        bld.CreateAndMigrateDatabase();
+                    });
+                });
+        }
+
+        public static void UseMadParbad(this IApplicationBuilder app)
+        {
+            app.UseParbadVirtualGateway();
+        }
+    }
+}
--- MadPay724.Payment/Startup.cs
+++ MadPay724.Payment/Startup.cs
@@ -1,39 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using MadPay724.Payment.Helpers.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace MadPay724.Payment
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-        public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             //services.AddRazorPages();
-
             services.AddMadDbContext();
             services.AddMadInitialize();
             services.AddAutoMapper(typeof(Startup));
             services.AddMadDI();
+
+            services.AddMadParbad(Configuration);
+
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMadExceptionHandle(env);
             app.UseMadInitialize();
+            app.UseMadParbad();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
