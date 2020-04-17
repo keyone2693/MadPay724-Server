25fd4ec89e672bd5c1fca36bffaae3d3b04b0445
key.one72@gmail.com <key.one72@gmail.com>
2020-04-17 17:32:39 +0430

    S65-E01

---
+++ MadPay724.Shop.Test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MadPay724.Shop.Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
